65e2bcc9f61eaf254293d09e92ab51799eabd966
KetanPatil18 <ketan.patil1304@gmail.com>
2022-10-04 16:33:08 +0530

    Calculator division commit

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,15 @@ package com.abc.miscellaneous;
 public class Calculator {
 int a=10;
 int b=20;
+int c;
 public int sum()
-{
-	return a+b;
+{ 
+        c=a+b;  
+	return c;
+}
+public int div()
+{ 
+        c=b-a;  
+	return c;
 }
 }
